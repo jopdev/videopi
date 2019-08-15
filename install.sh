582da93864f2e3c8a2a141d0f40e2686741ef236
JOPDev <juan.opegoretti@outlook.com>
2019-08-15 17:36:47 -0300

    Translate

--- install.sh
+++ install.sh
@@ -5,17 +5,17 @@ set -e
 
 # Make sure script is run as root.
 if [ "$(id -u)" != "0" ]; then
-  echo "Must be run as root with sudo! Try: sudo ./install.sh"
+  echo "Você precisa de permissão de administrador! Tente: sudo ./install.sh"
   exit 1
 fi
 
-echo "Installing dependencies..."
+echo "Instalando dependências..."
 echo "=========================="
 apt update && apt -y install git build-essential python3-dev python3 python3-pip python3-pygame supervisor omxplayer
 
 echo "Installing hello_video..."
 echo "========================="
-git clone https://github.com/adafruit/pi_hello_video.git
+git clone https://github.com/jopdev/hellopi.git
 cd pi_hello_video
 ./rebuild.sh
 cd hello_video
@@ -23,16 +23,16 @@ make install
 cd ../..
 rm -rf pi_hello_video
 
-echo "Installing video_looper program..."
+echo "Instalando Player..."
 echo "=================================="
 mkdir -p /mnt/usbdrive0 # This is very important if you put your system in readonly after
 pip3 install setuptools
 python3 setup.py install --force
 cp video_looper.ini /boot/video_looper.ini
 
-echo "Configuring video_looper to run on start..."
+echo "Configurando Player para iniciar automaticamente..."
 echo "==========================================="
 cp video_looper.conf /etc/supervisor/conf.d/
 service supervisor restart
 
-echo "Finished!"
+echo "Finalizado!"
